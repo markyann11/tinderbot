82da44d7d136c9968e8d973db5ecbee231261154
Mark Yann <mark.yann11@gmail.com>
2015-03-01 20:37:09 -0600

    add save user to csv

--- lib/tinderbot/bot.rb
+++ lib/tinderbot/bot.rb
@@ -1,17 +1,24 @@
 module Tinderbot
   class Bot
     attr_accessor :client
-
     def initialize(client)
       @client = client
+      require 'csv'
     end
 
     def like_recommended_users
       recommended_users = @client.recommended_users
-      while recommended_users
-        recommended_users.each { |user| @client.like user }
-        recommended_users = @client.recommended_users
+      CSV.open("mycsvfile.csv", "w") do |csv|
+        while recommended_users
+          recommended_users.each { |user|
+            @client.like user
+            csv << [user.id,user.name,user.bio,user.birth_date,user.photo_urls]
+          }
+          recommended_users = @client.recommended_users
+        end
+        #        csv << ["row", "of", "CSV", "data"]
       end
+
     end
   end
 end
